e3998953ac7a649b5b43cab369b4d3b1e49af986
Kyle Shannon <kyle@kmshannon.com>
2024-07-16 07:09:05 +0000

    micromamba changes to setup shell script - WIP

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -6,3 +6,17 @@ npm install -g yarn
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+
+# #!/bin/bash
+# # Install nvm for zshell
+# source ${NVM_DIR}/nvm.sh
+# nvm install 18
+# npm install -g yarn
+
+# micromamba create -y -f /home/vscode/workspace/.devcontainer/environment.yml
+# eval "$(micromamba shell hook --shell zsh)"
+# source /home/vscode/workspace/.devcontainer/.env
+# micromamba activate $MICROMAMBA_ENV
+# micromamba config append channels conda-forge
+# micromamba config append channels esri
